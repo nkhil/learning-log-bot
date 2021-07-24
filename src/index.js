b38036ce9712345d90973247cd63c9cf2221f120
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-07-24 16:07:35 +0100

    chore: add logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require('pino')();
 
 const app = express();
 
 app.get('/webhook', (req, res, next) => {
+  logger.info('Webhook request received');
   res.status(200).json();
 });
 
