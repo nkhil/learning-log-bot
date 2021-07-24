605d59c926e50b78c2501849eb331b80cd9a53a1
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-07-24 16:26:53 +0100

    feat: add liveness endpoint

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const logger = require('pino')();
 
 const app = express();
 
+app.get('/liveness', (req, res, next) => {
+  logger.info('liveness request received');
+  res.status(200).json({ status: 'OK' });
+});
+
 app.post('/webhook', (req, res, next) => {
   logger.info('Webhook request received');
-  res.status(200).json();
+  res.status(200).json({ status: 'OK' });
 });
 
 module.exports = app;
