8566ab188ee2bbf3491feb5d37140b58057449b0
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-07-24 16:25:24 +0100

    chore: change webhook endpoint from get to post

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const logger = require('pino')();
 
 const app = express();
 
-app.get('/webhook', (req, res, next) => {
+app.post('/webhook', (req, res, next) => {
   logger.info('Webhook request received');
   res.status(200).json();
 });
