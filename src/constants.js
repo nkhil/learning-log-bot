506c97b4f1c24a1eb85786ecb93434e35992e5cf
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-07-25 01:36:34 +0100

    feat: add some template responses

--- src/constants.js
+++ src/constants.js
@@ -10,4 +10,18 @@ module.exports = {
   },
   TEST_MESSAGE:
     'Hi, this is your learning log. Have you learned anything recently?',
+  messages: {
+    recurringQuestions: [
+      'Yours truly learning bot checking in!👋 Have you learned anything new recently?',
+      'Hey, have you learned anything interesting recently?',
+      "It's that time of day again. Have you been learning anything cool recently?",
+      'A journey of a thousand miles begins with a single step. Have you learned anything recently?',
+      "Hey you, yes you - how are you? How's the learning going?",
+    ],
+    positiveResponses: [
+      "Nice job ⭐⭐⭐⭐⭐ - I'll save this in the vault so it doesn't get lost in time",
+      "Well done. I'll record this in the log right away",
+    ],
+    negativeResponses: ["No worries, I'll be in touch soon!"],
+  },
 };
